af437e7dd3d3eaa3487f17fec6bb9c136ef22cf8
mohamad0880218 <115563288+mohamad0880218@users.noreply.github.com>
2023-11-28 11:38:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# Replace the following placeholders with your actual Azure Database for MySQL details
+database_username = "rizwan"
+database_password = "Mysql@3916"
+database_host = "final-project-sql.mysql.database.azure.com"
+database_port = 3306
+database_name = "{your_database}"
+#ssl_ca_filename = "{path/to/ca-cert-file.pem}"  # Replace with the path to your CA certificate file
+
+# Construct the SQLAlchemy Database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{database_username}:{database_password}@{database_host}:{database_port}/{database_name}?ssl_ca="{ca-cert filename}""
+
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -28,4 +39,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
-
