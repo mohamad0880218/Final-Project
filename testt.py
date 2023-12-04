edae493eda842cfad7a7264f7ce83c91d3453700
mohamad0880218 <115563288+mohamad0880218@users.noreply.github.com>
2023-12-04 00:33:54 -0500

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,8 +1,8 @@
 import unittest
 from main import app, db, User
 PASSWORD = "Root_User"
-PUBLIC_IP_ADDRESS = "34.105.101.145"
-DBNAME = "appdata"
+PUBLIC_IP_ADDRESS = "34.105.101.145:3306"
+DBNAME = "testappdb"
 PROJECT_ID = "grounded-pager-400600"
 INSTANCE_NAME = "my-sql"
 
